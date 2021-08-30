d627068dcf05ad8aed1d88ef5c813d430adda3c3
zhengzhipeng <847587146@qq.com>
2021-08-30 21:51:41 +0800

    zookeeper

--- customer04/src/main/java/com/street/lamp/customer/Customer04Main.java
+++ customer04/src/main/java/com/street/lamp/customer/Customer04Main.java
@@ -5,8 +5,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class Customer03Main {
+public class Customer04Main {
     public static void main(String[] args) {
-        SpringApplication.run(Customer03Main.class);
+        SpringApplication.run(Customer04Main.class);
     }
 }
